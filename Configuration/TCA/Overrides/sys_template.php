fb9beede0a2245e46060a09628f3038228aab115
Markus Klein <markus.klein@reelworx.at>
2023-04-27 19:23:05 +0200

    [TASK] Remove legacy TS

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,10 +1,4 @@
 <?php
 defined('TYPO3') || die('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('webcare', 'Configuration/TypoScript/basic', 'WebCare basic settings');
-
-if (version_compare(TYPO3_branch, '9.5', '>=')) {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('webcare', 'Configuration/TypoScript/cookie9', 'WebCare cookie consent');
-} else {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('webcare', 'Configuration/TypoScript/cookie8', 'WebCare cookie consent');
-}
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('webcare', 'Configuration/TypoScript/basic', 'WebCare cookie conset settings');
